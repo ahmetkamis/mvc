4e343dd71d8d34fb2375b843778f0ca82420b8f7
Ahmet Kamis <ahmet@Ahmets-MacBook-Pro.local>
2018-09-29 16:53:20 -0500

    2. commit

--- Core/Config.php
+++ Core/Config.php
@@ -9,8 +9,8 @@
 //DATABASE CONFIG!
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'spm4');
+define('DB_PASS', 'password');
+define('DB_NAME', 'test');
 define('DB_CHARSET', 'UTF-8');
 //define('DB_TIMEZONE_OFFSET', '-6:00'); //MYSQL TIME ZONE OFFSET  (-6:00 for chicago)
 define('DB_TIMEZONE_OFFSET', 'UTC'); //MYSQL TIME ZONE OFFSET  (0:00 for UTC)
